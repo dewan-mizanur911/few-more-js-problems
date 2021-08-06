e36c4f2061512c328e2d7959ba14c842e814f27c
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-07 01:08:08 +0600

    foobar

--- add-multiply.js
+++ add-multiply.js
@@ -6,3 +6,17 @@ function multply(num1, num2){
     const total = num1 * num2;
     return total;
 }
+function woodCalculator(table, chair, bed){
+    const perTableWood = 10;
+    const perChairWood = 3;
+    const perBedWood = 50;
+    // wood calculation
+    const tableWoodQuantity = perTableWood * table;
+    const chairWoodQuantity = perChairWood * chair;
+    const perBedWoodQuantity = perBedWood * bed;
+    // adding all woods
+    const totalWood = tableWoodQuantity + chairWoodQuantity + perBedWoodQuantity;
+    return totalWood;
+}
+const furnitureWood = woodCalculator(1, 3, 2);
+console.log(furnitureWood);
